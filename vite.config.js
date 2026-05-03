cbc593bd749d050f0e13ec60c202c2d965f8d77e
Skylar Grow <dylangrow@gmail.com>
2026-05-02 20:01:12 -0400

    Simplify Vite config and specify publicDir
    Removed server and build configurations, added publicDir.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Ensures the build assumes the sub-folder path of GitHub Pages
   base: '/carecompass-lite/',
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  }
+  // Explicitly tells Vite where your static assets are
+  publicDir: 'public',
 })
